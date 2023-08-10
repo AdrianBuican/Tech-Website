e9391efcfafdecdb7cd1f564a380dab9d31386c1
Adrian Buican <adrian.buican7@gmail.com>
2023-08-10 05:54:27 +0300

    home page layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
+import styles from './page.module.css'
 import Image from 'next/image'
 import Hero from "public/hero.png"
 
 export default function Home() {
   return (
-    <div>
-      <Image src={Hero} alt="" />
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Powerful designs for your digital products
+        </h1>
+        <p className={styles.description}>
+          We bring your ideas to life, with the best teams from the global tech industry.
+        </p>
+        <button className={styles.button}>See Our Work</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
   )
 }

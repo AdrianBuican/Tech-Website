84fc1184728961bf3acc1c966e2469105d348fee
Adrian Buican <adrian.buican7@gmail.com>
2023-08-18 00:55:16 +0300

    Dark/Light Mode

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,13 +3,22 @@ import styles from './page.module.css'
 
 const BlogPost = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Article</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus provident temporibus aliquid maxime expedita, quae dolor sint, quia minima corrupti quibusdam mollitia facere laboriosam aut exercitationem, fugit tempore voluptatem illo.</p>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur assumenda aspernatur eius odit harum praesentium corporis placeat unde nulla quaerat saepe aliquam, quibusdam inventore, quos rem perspiciatis! Quas, dolor commodi.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod obcaecati aut pariatur sapiente, deserunt, labore inventore nemo vitae, nisi voluptatibus nulla esse excepturi modi ea qui deleniti iure numquam aliquam!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facilis atque maxime corporis. Laudantium necessitatibus tempore, dolores libero deleniti aperiam placeat culpa possimus illum reiciendis animi voluptate aut voluptates atque.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit expedita laudantium ullam odit unde distinctio cum hic, quas nesciunt facere animi sit possimus explicabo, excepturi accusamus facilis aut culpa!</p>
+    <div>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Article Title</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, natus!</h2>
+        <h3>- by John Doe -</h3>
+      </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus provident temporibus aliquid maxime expedita, quae dolor sint, quia minima corrupti quibusdam mollitia facere laboriosam aut exercitationem, fugit tempore voluptatem illo.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur assumenda aspernatur eius odit harum praesentium corporis placeat unde nulla quaerat saepe aliquam, quibusdam inventore, quos rem perspiciatis! Quas, dolor commodi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod obcaecati aut pariatur sapiente, deserunt, labore inventore nemo vitae, nisi voluptatibus nulla esse excepturi modi ea qui deleniti iure numquam aliquam!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facilis atque maxime corporis. Laudantium necessitatibus tempore, dolores libero deleniti aperiam placeat culpa possimus illum reiciendis animi voluptate aut voluptates atque.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit expedita laudantium ullam odit unde distinctio cum hic, quas nesciunt facere animi sit possimus explicabo, excepturi accusamus facilis aut culpa!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus provident temporibus aliquid maxime expedita, quae dolor sint, quia minima corrupti quibusdam mollitia facere laboriosam aut exercitationem, fugit tempore voluptatem illo.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur assumenda aspernatur eius odit harum praesentium corporis placeat unde nulla quaerat saepe aliquam, quibusdam inventore, quos rem perspiciatis! Quas, dolor commodi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod obcaecati aut pariatur sapiente, deserunt, labore inventore nemo vitae, nisi voluptatibus nulla esse excepturi modi ea qui deleniti iure numquam aliquam!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facilis atque maxime corporis. Laudantium necessitatibus tempore, dolores libero deleniti aperiam placeat culpa possimus illum reiciendis animi voluptate aut voluptates atque.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit expedita laudantium ullam odit unde distinctio cum hic, quas nesciunt facere animi sit possimus explicabo, excepturi accusamus facilis aut culpa!</p>
     </div>
   )
 }
